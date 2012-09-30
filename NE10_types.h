6f70e715b0f9a0d5f201ae1529b0b963288b3218
Elene Trull <endahner@ncsu.edu>
2012-09-30 17:52:14 -0400

    removed extra stuff from NE10_types.h in hopes that it would help with the strange time difference, but no luck

--- NE10_types.h
+++ NE10_types.h
@@ -25,9 +25,6 @@
 #ifndef NE10_TYPES_H
 #define NE10_TYPES_H
 
-#include <stdio.h>
-#include <assert.h>
-
 /////////////////////////////////////////////////////////
 // constant values that are used across the library
 /////////////////////////////////////////////////////////
@@ -49,117 +46,5 @@ typedef unsigned long long int  ne10_uint64_t;
 typedef float                   ne10_float32_t;
 typedef double                  ne10_float64_t;
 typedef int                     ne10_result_t;     // resulting [error-]code
-
-typedef struct
-{
-        ne10_float32_t x;
-        ne10_float32_t y;
-} ne10_vec2f_t; // a 2-tuple of ne10_float32_t values
-
-typedef struct
-{
-        ne10_float32_t x;
-        ne10_float32_t y;
-        ne10_float32_t z;
-} ne10_vec3f_t; // a 3-tuple of ne10_float32_t values
-
-typedef struct
-{
-        ne10_float32_t x;
-        ne10_float32_t y;
-        ne10_float32_t z;
-        ne10_float32_t w;
-} ne10_vec4f_t; // a 4-tuple of ne10_float32_t values
-
-
-typedef struct { ne10_float32_t r1; ne10_float32_t r2; } __attribute__((packed)) ne10_mat_row2f;
-
-typedef struct
-{
-        ne10_mat_row2f c1;
-        ne10_mat_row2f c2;
-
-} __attribute__((packed)) ne10_mat2x2f_t;     // a 2x2 matrix
-
-static inline void createColumnMajorMatrix2x2( ne10_mat2x2f_t * outMat, ne10_float32_t m11, ne10_float32_t m21, ne10_float32_t m12, ne10_float32_t m22)
-{
-   assert( NULL != outMat );
-
-    outMat->c1.r1 = m11;
-    outMat->c1.r2 = m21;
-    outMat->c2.r1 = m12;
-    outMat->c2.r2 = m22;
-}
-
-
-typedef struct { ne10_float32_t r1; ne10_float32_t r2; ne10_float32_t r3; } __attribute__((packed)) ne10_mat_row3f;
-
-typedef struct
-{
-        ne10_mat_row3f c1;
-        ne10_mat_row3f c2;
-        ne10_mat_row3f c3;
-
-} __attribute__((packed)) ne10_mat3x3f_t;     // a 3x3 matrix
-
-static inline void createColumnMajorMatrix3x3( ne10_mat3x3f_t * outMat, ne10_float32_t m11, ne10_float32_t m21, ne10_float32_t m31,
-                                                                       ne10_float32_t m12, ne10_float32_t m22, ne10_float32_t m32,
-                                                                       ne10_float32_t m13, ne10_float32_t m23, ne10_float32_t m33)
-{
-    assert( NULL != outMat );
-
-    outMat->c1.r1 = m11;
-    outMat->c1.r2 = m21;
-    outMat->c1.r3 = m31;
-
-    outMat->c2.r1 = m12;
-    outMat->c2.r2 = m22;
-    outMat->c2.r3 = m32;
-
-    outMat->c3.r1 = m13;
-    outMat->c3.r2 = m23;
-    outMat->c3.r3 = m33;
-}
-
-
-typedef struct { ne10_float32_t r1; ne10_float32_t r2; ne10_float32_t r3; ne10_float32_t r4; } __attribute__((packed)) ne10_mat_row4f;
-
-typedef struct
-{
-        ne10_mat_row4f c1;
-        ne10_mat_row4f c2;
-        ne10_mat_row4f c3;
-        ne10_mat_row4f c4;
-
-} __attribute__((packed)) ne10_mat4x4f_t;     // a 4x4 matrix
-
-static inline void createColumnMajorMatrix4x4( ne10_mat4x4f_t * outMat, ne10_float32_t m11, ne10_float32_t m21, ne10_float32_t m31, ne10_float32_t m41,
-                                                                       ne10_float32_t m12, ne10_float32_t m22, ne10_float32_t m32, ne10_float32_t m42,
-                                                                       ne10_float32_t m13, ne10_float32_t m23, ne10_float32_t m33, ne10_float32_t m43,
-                                                                       ne10_float32_t m14, ne10_float32_t m24, ne10_float32_t m34, ne10_float32_t m44)
-{
-    assert( NULL != outMat );
-
-    outMat->c1.r1 = m11;
-    outMat->c1.r2 = m21;
-    outMat->c1.r3 = m31;
-    outMat->c1.r4 = m41;
-
-    outMat->c2.r1 = m12;
-    outMat->c2.r2 = m22;
-    outMat->c2.r3 = m32;
-    outMat->c2.r4 = m42;
-
-    outMat->c3.r1 = m13;
-    outMat->c3.r2 = m23;
-    outMat->c3.r3 = m33;
-    outMat->c3.r4 = m43;
-
-    outMat->c4.r1 = m14;
-    outMat->c4.r2 = m24;
-    outMat->c4.r3 = m34;
-    outMat->c4.r4 = m44;
-}
-
 
 #endif
--- readme.txt
+++ readme.txt
@@ -1,3 +1,5 @@
+endahner [Elene Trull]
+
 How to use:
 To build the original source (with flags which make it pretty fast), 
 simply use make (or make ORIGINAL=1)
@@ -5,4 +7,5 @@ To build with neon assembly I wrote, use make NEON=1
 
 Unfortunately, it seems I'm not as good as the compiler. Part of it 
 seems to be due to some overhead included with the other files being 
-linked. 
+linked, as I experienced it slowing the original code when all files 
+were linked, but only the original code was compiled.
